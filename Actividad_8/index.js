be7bb05c6e301c1e65969ad55946955f886290c8
Guido Cuccioletti <guidocucciolettit2@gmail.com>
2025-08-27 19:52:32 -0300

    Se agrega la actividad 8

---
+++ Actividad_8/index.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let alumnos = [
+    { nombre: "Paragua", notas: [8, 7, 9] },
+    { nombre: "Fabian", notas: [5, 6, 4] },
+    { nombre: "Pedro", notas: [2, 3] }
+];
+function calcularPromedio(notas) {
+    let suma = 0;
+    for (let i = 0; i < notas.length; i++) {
+        suma += notas[i];
+    }
+    return suma / notas.length;
+}
+function notaFinal() {
+    for (let i = 0; i < alumnos.length; i++) {
+        let promedio = calcularPromedio(alumnos[i].notas);
+        let nombre = alumnos[i].nombre;
+        if (promedio >= 7) {
+            console.log(`${nombre} promocionó con un promedio de ${promedio.toFixed(2)}`);
+        }
+        else if (promedio >= 4) {
+            console.log(`${nombre} aprobó con un promedio de ${promedio.toFixed(2)}`);
+        }
+        else {
+            console.log(`${nombre} desaprobó con un promedio de ${promedio.toFixed(2)}`);
+        }
+    }
+}
+notaFinal();
